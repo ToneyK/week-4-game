af4114c6c790ea56d5bbb144863c54edd3c10896
Your Name <you@example.com>
2018-02-16 14:02:14 -0700

    fixed

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,7 @@
+$(function () {
+
 //VARIABLES
+var computerRandomNumber;
 //wins
 var wins = 0;
 //losses
@@ -6,40 +9,63 @@ var losses = 0;
 //current user value
 var userValue = 0;
 
-//function reset (){
-// 	var computerRandomNumber = Math.floor((Math.random() * 100) + 20);
-// 	$("#computerRandom").html("Number to Guess: " + computerRandomNumber);
-// };
+	function reset (){
+		targetNumber();
+		newCrystalValues();
+		userValue = 0;
+		$("#userScore").html("Your Score: " + userValue);
+	};
 
-$(function targetNumber() {
-	//computer random number
-	var computerRandomNumber = Math.floor((Math.random() * 100) + 20);
-	$("#computerRandom").html("Number to Guess: " + computerRandomNumber);
+	function targetNumber() {
+		//computer random number
+		computerRandomNumber = Math.floor((Math.random() * 100) + 20);
+		$("#computerRandom").html("Number to Guess: " + computerRandomNumber);
+	}; // END TARGETNUMBER FUNCTION
+
+	targetNumber();
+
+	// var names = ["Toney", "Manny"];
+	// var ages = [27, 30];
+	// function printStats(name, age) {
+	// 	console.log(name + " is " + age + " years old.")
+	// }
+  //
+	// printStats(names[1], ages[1]);
+
+
 
 	//value for each click of crystal
-	$("img").each(function crystalValue() {
-		$(this).attr("data-value", Math.floor((Math.random() * 11) + 1));
-		console.log(this)
-	}); // END OF FUNCTION
+	function newCrystalValues () {
+		$("img").each(function () {
+			$(this).attr("data-value", Math.floor((Math.random() * 11) + 1));
+			console.log(this)
+		}); // END OF EACH FUNCTION TO APPLY CRYSTAL VALUES
+	}; // END OF newCrystalValues FUNCTION
+
+	newCrystalValues();
+
+
 
 	//on click for userValue
 	$("img").on("click", function () {
 		userValue += parseInt($(this).attr("data-value"));
 		//display userValue
 		$("#userScore").html("Your Score: " + userValue);
-		
+
 		//wins are update to html
 		if (userValue === computerRandomNumber) {
 			wins++;
 			$("#wins").html("Wins: " + wins);
+			reset();
 		}
 
 		//losses are update to html
-		if(userValue>computerRandomNumber){
+		if (userValue > computerRandomNumber) {
 			losses++;
-		$("#losses").html("Losses: " + losses);
+			$("#losses").html("Losses: " + losses);
+			reset();
 		}
-	});//END OF FUNCTION
+	});//END OF CLICK FUNCTION TO COMPARE CRYSTAL VALUES
 
 
 
